6e2d1c36444936ff34f4973ebc04dfad8ffe42d6
Harsha Velagala <your-email@example.com>
2025-11-27 23:29:31 +0530

    Remove fake backend

--- backend/src/main/java/com/rideshare/repository/RideRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rideshare.repository;
-
-import com.rideshare.model.Ride;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RideRepository extends JpaRepository<Ride, Long> {
-}
